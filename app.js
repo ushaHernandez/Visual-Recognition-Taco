0b526a08c97d9c2d7315f060a3aef1fc66e8af64
ushaHernandez <ursunina@hotmail.com>
2019-08-01 20:39:40 -0500

    cambio

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var fs = require('fs');
 //set IBM cloud credentials in this section
 var visualRecognition = new VisualRecognitionV3({
   version: '2019-05-29',
-  iam_apikey: 'Remplace aqui'
+  iam_apikey:"WcjLtTe-4wmxPr4JDU7sY6c0kgPahXMfFpC7D0xjHSAg"
 });
 
 // Set The Storage Engine
@@ -72,7 +72,7 @@ app.post('/upload', (req, res) => {
         });
       } else {
         var images_file = fs.createReadStream(`./public/uploads/${req.file.filename}`);
-        var classifier_ids = ["Tiposdetaco_2107577272"];
+        var classifier_ids = ["pau-asistent_144111699"];
         console.log("datos de la imagen -- " + images_file);
         //
           var params = {
